70f1cf82bef8b270b4f84d9cc8c76f6eaf12066d
HasegawaTaizou <caiocoghi@gmail.com>
2024-06-25 14:35:13 -0300

    alo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:website/utils/launch_url.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,32 +13,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              ElevatedButton(
-                onPressed: () async {
-                  await launchUrl(
-                    Uri.parse("https://www.google.com/"),
-                    mode: LaunchMode.externalApplication,
-                  );
-                },
-                child: const Text('Link google externo'),
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  LaunchURL.launch(context, "https://www.google.com/");
-                },
-                child: const Text('Link google interno'),
-              ),
-            ],
-          ),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: MyHome());
+  }
+}
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                await launchUrl(
+                  Uri.parse("https://www.google.com/"),
+                  mode: LaunchMode.externalApplication,
+                );
+              },
+              child: const Text('Link google externo'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                LaunchURL.launch(context, "https://www.google.com/");
+              },
+              child: const Text('Link google interno'),
+            ),
+          ],
         ),
       ),
     );
